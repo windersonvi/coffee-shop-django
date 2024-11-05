6fbe16316e8ff5dc641c60b2da4536d46d541030
windersonvi <windersonvo@gmail.com>
2024-11-05 00:13:32 -0400

    Agregar vista de lista de productos y plantilla correspondiente

--- coffe_shop/products/views.py
+++ coffe_shop/products/views.py
@@ -1,6 +1,8 @@
 #from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+
+from .models import Product
 from .forms import ProductForm
 
 class ProductFormView(generic.FormView):
@@ -11,3 +13,8 @@ class ProductFormView(generic.FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+class ProductListView(generic.ListView):
+    template_name = 'products/list_product.html'
+    model = Product
+    context_object_name = 'products'
